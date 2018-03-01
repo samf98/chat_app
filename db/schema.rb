3b6d03980937844cd4043cbbde79f9c0ca8cfe0c
samf98 <samir.facusse@unitec.edu>
2018-02-28 18:45:55 -0600

    Boop

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20180228053132) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
